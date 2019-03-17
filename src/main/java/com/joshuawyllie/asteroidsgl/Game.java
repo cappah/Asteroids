f645ad85335988e562d052a1d33bc074ebf53422
j-wyllie <joshuawyllie0@gmail.com>
2019-03-17 17:43:57 +0100

    Initial commit

---
+++ src/main/java/com/joshuawyllie/asteroidsgl/Game.java
@@ -0,0 +1,50 @@
+package com.joshuawyllie.asteroidsgl;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.util.AttributeSet;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class Game extends GLSurfaceView implements GLSurfaceView.Renderer {
+    private static final int BG_COLOUR = Color.rgb(135, 206, 235);
+
+    public Game(Context context) {
+        super(context);
+        init();
+    }
+
+    public Game(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        setEGLContextClientVersion(2);
+        setRenderer(this);
+    }
+
+    @Override
+    public void onSurfaceCreated(final GL10 unused, final EGLConfig config) {
+        //build program (shaders)
+        //tell opengl to use program (our shaders)
+        float red = Color.red(BG_COLOUR) / 255f;
+        float green = Color.green(BG_COLOUR) / 255f;
+        float blue = Color.blue(BG_COLOUR) / 255f;
+        float alpha = 1f;
+        GLES20.glClearColor(red, green, blue, alpha);
+    }
+
+    @Override
+    public void onSurfaceChanged(final GL10 unused, final int width, final int height) {
+
+    }
+
+    @Override
+    public void onDrawFrame(final GL10 unused) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+    }
+}
---
+++ src/main/java/com/joshuawyllie/asteroidsgl/MainActivity.java
@@ -0,0 +1,37 @@
+package com.joshuawyllie.asteroidsgl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    Game game;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        game = new Game(this);
+        setContentView(game);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (!hasFocus) {
+            return;
+        }
+        setFullScreen();
+    }
+
+    private void setFullScreen() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
+    }
+}
